8336aec5e3f1ae79b3388a3b49e12c20e6c0c7f4
ZelTs <zeltsmx@outlook.com>
2024-05-06 18:05:29 -0600

    Funcionalidad de listas

--- assets/js/states.js
+++ assets/js/states.js
@@ -48,7 +48,104 @@ function cargarMunicipios() {
                 if (estado.municipios.length > 0) {
                     municipioSelect.value = estado.municipios[0];
                 }
+
+                // Una vez cargados los municipios, llamar a cargarFechasYHoras
+                cargarFechasYHoras();
             })
             .catch(error => console.error('Error al obtener los datos:', error));
     }
+}
+
+
+function cargarFechasYHoras() {
+    const estadoSelect = document.getElementById("estado");
+    const municipioSelect = document.getElementById("municipio");
+    const estadoSeleccionado = estadoSelect.value;
+    const municipioSeleccionado = municipioSelect.value;
+
+    // Obtener las fechas y horas correspondientes al estado y municipio seleccionados
+    fetch('assets/JSON/data.json')
+        .then(response => response.json())
+        .then(data => {
+            const estadoData = data[estadoSeleccionado]; // Acceder a los datos de Aguascalientes
+            if (estadoData) {
+                const municipioData = estadoData.find(mun => mun.nombre === municipioSeleccionado);
+                if (municipioData) {
+                    const coordenadas = municipioData.coordenadas.split(','); // Separar las coordenadas
+                    const latitud = parseFloat(coordenadas[0]); // Convertir la latitud a número
+
+                    // Verificar si la latitud está fuera de la zona intertropical
+                    if (latitud > 23.4380) {
+                        // Mostrar mensaje de advertencia
+                        const lugarEvento = document.querySelector('.place__event');
+                        lugarEvento.innerHTML = `
+                                <div class="sc__localidad">
+                                    <span class="title__localidad">Aviso</span>
+                                    <span class="dato__event">Este fenómeno no puede ser observado en esta área debido a que el municipio está fuera de la zona intertropical</span>
+                                </div>
+                            `;
+                        return; // Detener la ejecución de la función
+                    }
+
+                    // Si la latitud está dentro de la zona intertropical, continuar con la carga de fechas y horas
+                    const fecha1 = municipioData.fechas.Fecha1;
+                    const fecha2 = municipioData.fechas.Fecha2;
+                    const hora1 = municipioData.horas.Hora1;
+                    const hora2 = municipioData.horas.Hora2;
+
+                    // Obtener el nombre del estado
+                    fetch('assets/JSON/mp.json')
+                        .then(response => response.json())
+                        .then(estados => {
+                            const estadoNombre = estados.find(est => est.clave === estadoSeleccionado).nombre;
+
+                            // Mostrar las fechas y horas en la página con el nombre del estado
+                            const lugarEvento = document.querySelector('.place__event');
+                            lugarEvento.innerHTML = `
+                                    <div class="sc__localidad">
+                                        <span class="title__localidad">Latitud y Longitud</span>
+                                        <span class="dato__event">${coordenadas}</span>
+                                    </div>                            
+                                    <div class="sc__localidad">
+                                        <span class="title__localidad">Estado</span>
+                                        <span class="dato__event">${estadoNombre}</span>
+                                    </div>
+                                    <div class="sc__localidad">
+                                        <span class="title__localidad">Municipio</span>
+                                        <span class="dato__event">${municipioSeleccionado}</span>
+                                    </div>
+                                    <div class="sc__localidad">
+                                        <span class="title__localidad">Fecha y Hora</span>
+                                        <span class="dato__event">${fecha1} - ${hora1} hrs</span>
+                                    </div>
+                                    <div class="sc__localidad">
+                                        <span class="title__localidad">Fecha y Hora</span>
+                                        <span class="dato__event">${fecha2} - ${hora2} hrs</span>
+                                    </div>
+                                `;
+                        })
+                        .catch(error => console.error('Error al obtener los datos de los estados:', error));
+                } else {
+                    // Si no se encuentra el estado seleccionado, mostrar un aviso
+                    const lugarEvento = document.querySelector('.place__event');
+                    lugarEvento.innerHTML = `
+                        <div class="sc__localidad">
+                            <span class="title__localidad">Aviso</span>
+                            <span class="dato__event">Datos no encontrados para este Estado</span>
+                        </div>
+                    `;
+
+                }
+            } else {
+                // Si no se encuentra el municipio seleccionado, mostrar un aviso
+                const lugarEvento = document.querySelector('.place__event');
+                lugarEvento.innerHTML = `
+                        <div class="sc__localidad">
+                            <span class="title__localidad">Aviso</span>
+                            <span class="dato__event">Datos no encontrados para este Municipio</span>
+                        </div>
+                    `;
+            }
+        })
+        .catch(error => console.error('Error al obtener los datos de fechas y horas:', error));
 }
